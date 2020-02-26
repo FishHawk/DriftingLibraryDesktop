8874e19fd8471c715c4880ae0198e84b5126a1e7
FishHawk <whang177@gmail.com>
2020-02-26 15:08:43 +0800

    Clean library model code

--- src/model/library_model.cpp
+++ src/model/library_model.cpp
@@ -1,20 +1,39 @@
 #include "library_model.hpp"
 
-#include <QCollator>
-#include <QDebug>
-#include <QDir>
-#include <QDirIterator>
-
-#include "model/util.hpp"
-
 using model::LibraryModel;
 
 LibraryModel::LibraryModel(QUrl url)
-    : m_url(url){};
+    : m_url(url) {}
 
 LibraryModel::~LibraryModel() {
     clear();
-};
+}
+
+int LibraryModel::rowCount(const QModelIndex &) const {
+    return m_mangas.size();
+}
+
+QVariant LibraryModel::data(const QModelIndex &index, int role) const {
+    if (!index.isValid() || index.row() < 0 || index.row() >= m_mangas.size())
+        return QVariant();
+
+    if (role == MangaSummaryRole::IdRole)
+        return QVariant::fromValue(m_mangas.at(index.row())->id);
+    else if (role == MangaSummaryRole::TitleRole)
+        return QVariant::fromValue(m_mangas.at(index.row())->title);
+    else if (role == MangaSummaryRole::ThumbRole)
+        return QVariant::fromValue(m_mangas.at(index.row())->thumb);
+    else
+        return QVariant();
+}
+
+QHash<int, QByteArray> LibraryModel::roleNames() const {
+    QHash<int, QByteArray> roles;
+    roles[MangaSummaryRole::IdRole] = "mangaId";
+    roles[MangaSummaryRole::TitleRole] = "title";
+    roles[MangaSummaryRole::ThumbRole] = "thumb";
+    return roles;
+}
 
 void LibraryModel::clear() {
     for (auto manga : m_mangas) {
--- src/model/library_model.hpp
+++ src/model/library_model.hpp
@@ -2,19 +2,12 @@
 #define MODEL_LIBRARY_MODEL_HPP
 
 #include <QAbstractListModel>
-#include <QObject>
 #include <QUrl>
 
 #include "model/manga_model.hpp"
 #include "model/tag_filter.hpp"
 
 namespace model {
-
-struct MangaSummary {
-    QString id;
-    QString title;
-    QUrl thumb;
-};
 
 class LibraryModel : public QAbstractListModel {
     Q_OBJECT
@@ -29,42 +22,25 @@ public:
     explicit LibraryModel(QUrl library_address);
     virtual ~LibraryModel();
 
-    int rowCount(const QModelIndex &) const override {
-        return m_mangas.size();
-    }
-
-    QVariant data(const QModelIndex &index, int role) const override {
-        if (!index.isValid() || index.row() < 0 || index.row() >= m_mangas.size())
-            return QVariant();
-
-        if (role == MangaSummaryRole::IdRole)
-            return QVariant::fromValue(m_mangas.at(index.row())->id);
-        else if (role == MangaSummaryRole::TitleRole)
-            return QVariant::fromValue(m_mangas.at(index.row())->title);
-        else if (role == MangaSummaryRole::ThumbRole)
-            return QVariant::fromValue(m_mangas.at(index.row())->thumb);
-        else
-            return QVariant();
-    }
+    int rowCount(const QModelIndex &) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
 
     Q_INVOKABLE virtual void query(QString patterns) = 0;
     virtual MangaModel *openManga(QString id) = 0;
 
 protected:
-    QHash<int, QByteArray> roleNames() const override {
-        QHash<int, QByteArray> roles;
-        roles[MangaSummaryRole::IdRole] = "mangaId";
-        roles[MangaSummaryRole::TitleRole] = "title";
-        roles[MangaSummaryRole::ThumbRole] = "thumb";
-        return roles;
+    struct MangaSummary {
+        QString id;
+        QString title;
+        QUrl thumb;
     };
 
     void clear();
 
-    int m_size;
     QUrl m_url;
-    TagFilter m_tag_filter;
     std::vector<MangaSummary *> m_mangas;
+    TagFilter m_tag_filter;
 };
 
 } // namespace model
